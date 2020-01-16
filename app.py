f98350354212408ee02a3e77d5bc45f2d2e10894
Jpcp98 <55485854+Jpcp98@users.noreply.github.com>
2020-01-16 00:40:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ data_options = [{'label': 'Arrivals', 'value': 'Arrivals'},
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
 
